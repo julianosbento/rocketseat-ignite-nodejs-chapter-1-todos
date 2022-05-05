a4e38174ca5c6df5adfe24310e64ead359709be0
julianosbento <js.bentocontato@gmail.com>
2022-05-04 22:41:41 -0300

    feat: add user creation route

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,43 @@
 import express from 'express';
 import cors from 'cors';
 
-// import { v4: uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+type User = {
+  id: string;
+  name: string;
+  username: string;
+  todos: Array<any>;
+}
+
+const users: Array<User> = [];
 
 function checksExistsUserAccount() {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userExists = users.find((user) => user.name === name);
+
+  if (userExists) response.status(400).json({ error: 'User already exists!' });
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  response.status(201).json(user);
 });
 
 app.get('/todos', (request, response) => {
